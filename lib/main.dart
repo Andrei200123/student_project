95aa0fe77fd364c505e20b53478e2da35b05cabb
Andrei200123 <andrei-vorontsoff2013@yandex.ru>
2022-04-07 22:07:44 +0300

    Первый коммит)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('Test'),
-));
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.amberAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Student project"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            "Test Student",
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.green,
+                fontFamily: 'RubikWetPaint'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("");
+          },
+          child: Text("Тык"),
+        ),
+      ),
+    );
+  }
+}
